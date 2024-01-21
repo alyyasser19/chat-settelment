bd1a7d409a8da4f7967fd417072e1a67fa656076
“alyhegazy19” <51823470+alyyasser19@users.noreply.github.com>
2024-01-21 18:38:26 +0200

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import DefaultLayout from "@/components/DefaultLayout";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +24,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+                          <Script
+                        id="gtag-base"
+                        strategy="afterInteractive"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-TP2T3Q4B')
+          `
+                        }}
+                    />
       <body className={inter.className} suppressHydrationWarning={true}>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TP2T3Q4B"
+height="0" width="0"></iframe></noscript>
+
         <DefaultLayout>{children}</DefaultLayout>
       </body>
     </html>
