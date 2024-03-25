d097afa92f8dffa72a051da65fa67d8b8bd19a80
“alyhegazy19” <51823470+alyyasser19@users.noreply.github.com>
2024-03-25 16:51:04 +0200

    add test chatbot

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
       <ProductList />
+      <script
+            src="http://localhost:3000/embed.min.js"
+            id="3W_BdOIqpRDXL03G6Xg_7"
+            defer
+        ></script>
     </main>
   );
 }
